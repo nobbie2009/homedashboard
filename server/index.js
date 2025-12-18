02e998e258d08a94796af467ab0fe37b52e35e06
Norbert Jahn <nobbie2009@gmail.com>
2025-12-18 22:01:02 +0100

    fix nginx

--- server/index.js
+++ server/index.js
@@ -1,12 +1,15 @@
 import express from 'express';
 import cors from 'cors';
-import { Edupage } from 'edupage-api';
 import { google } from 'googleapis';
 import dotenv from 'dotenv';
 import fs from 'fs';
 import path from 'path';
+import { fileURLToPath } from 'url';
 
 dotenv.config();
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 app.use(cors());
