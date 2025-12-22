1adbf862c524d29f72f1fb9a678612a1f2184725
Norbert Jahn <nobbie2009@gmail.com>
2025-12-22 13:30:06 +0100

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,13 @@ import react from '@vitejs/plugin-react'
 import path from 'path'
 import { execSync } from 'child_process'
 
-const commitHash = execSync('git rev-parse --short HEAD').toString().trim();
+let commitHash = '';
+try {
+  commitHash = execSync('git rev-parse --short HEAD').toString().trim();
+} catch (error) {
+  console.warn('Git commit hash could not be retrieved:', error);
+  commitHash = 'unknown';
+}
 
 // https://vitejs.dev/config/
 export default defineConfig({
